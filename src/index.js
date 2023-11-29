4eb09fb03751ed5e0fb2d108d0fe52fd7305d335
inhasid <sidorchuk.inga@gmail.com>
2023-11-29 22:53:16 +0100

    commit6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import { RequestImage } from "./js/back";
 import createMarkup from "./js/markup"
-import axios from "axios";
 import Notiflix from 'notiflix';
 
 
@@ -7,59 +7,26 @@ const refs = {
     searchForm: document.querySelector(".search-form"),
     gallery: document.querySelector(".gallery"),
     loadMoreBtn: document.querySelector(".load-more"),
+    api: new RequestImage(),
 };
 
-const { searchForm, gallery, loadMoreBtn } = refs;
-
-const BASE_URL = "https://pixabay.com/api/";
-const API_KEY = "40892694-b915ef9d6feb731a5d3c7d944";
+const { searchForm, gallery, loadMoreBtn, api } = refs;
 
 loadMoreBtn.classList.add('is-hidden');
 
-let page = 1;
+//let page = 1;
 let dataUserRes = '';
+let endArr = 0;
 
 searchForm.addEventListener('submit', handleSubmitImages);
 loadMoreBtn.addEventListener('click', handleClickLoadMore);
 
 function handleClickLoadMore() {
+    page += 1;
+    api.page = page.toString();
     loadMoreBtn.classList.add('is-hidden');
-    serviceImage();
-}
+    api.serviceImage(dataUserRes).then(respImage => {
 
-async function handleSubmitImages(event) {
-    event.preventDefault();
-    
-    const userData = event.target[0].value;
-    console.log(userData);
-    dataUserRes = userData.trim();
-
-   // userData = '';
-    if (!dataUserRes) {
-        Notiflix.Notify.failure('Please enter your request');
-        return;
-    }
-
-    page = 1;
-    gallery.innerHTML = '';
-    await serviceImage();
-}
-
-
-async function serviceImage() {
-
-    const params = new URLSearchParams({
-                key: `${API_KEY}`,
-                q: `${dataUserRes}`,
-             //   image_type: 'photo',
-             //  orientation: 'horizontal',
-             //   safesearch: 'true',
-    });
-    
-    try {
-        const respImage = await axios.get(
-            `${BASE_URL}?key=${API_KEY}&q=${dataUserRes}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-        );
         const { totalHits, hits } = respImage.data;
         
         if (hits.length === 0) {
@@ -67,12 +34,52 @@ async function serviceImage() {
             Notiflix.Notify.warning(
                 'Sorry, there are no images matching your search query. Please try again.'
             );
+            searchForm.reset();
             return;
         }
             
         gallery.insertAdjacentHTML("beforeend", createMarkup(hits));
 
-        if (page * 40 <= totalHits) {
+        if (page * 40 < totalHits) {
+            loadMoreBtn.classList.remove('is-hidden');
+
+        } else {
+            loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+        }
+    });
+}
+
+
+async function handleSubmitImages(event) {
+   event.preventDefault();
+    
+    const userData = event.target[0].value;
+    dataUserRes = userData.trim();
+
+    if (!dataUserRes) {
+        Notiflix.Notify.failure('Please enter your request');
+        return;
+    }
+
+    page = 1;
+    gallery.innerHTML = '';
+    api.serviceImage(dataUserRes).then(respImage => {
+
+        const { totalHits, hits } = respImage.data;
+        
+        if (hits.length === 0) {
+            loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.warning(
+                'Sorry, there are no images matching your search query. Please try again.'
+            );
+            searchForm.reset();
+            return;
+        }
+            
+        gallery.insertAdjacentHTML("beforeend", createMarkup(hits));
+
+        if (page * 40 < totalHits) {
             loadMoreBtn.classList.remove('is-hidden');
 
         } else {
@@ -80,12 +87,47 @@ async function serviceImage() {
             Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
         }
         page += 1;
-    }
-    catch (err) {
-        console.log(err);
-        Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
-    }
-    finally {
-        searchForm.reset();
-    }
+    });
 }
+
+
+// async function serviceImage() {
+
+//     const params = new URLSearchParams({
+//                 key: `${API_KEY}`,
+//                 q: `${dataUserRes}`,
+//     });
+    
+//     try {
+//         const respImage = await axios.get(
+//             `${BASE_URL}?key=${API_KEY}&q=${dataUserRes}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+//         );
+//         const { totalHits, hits } = respImage.data;
+        
+//         if (hits.length === 0) {
+//             loadMoreBtn.classList.add('is-hidden');
+//             Notiflix.Notify.warning(
+//                 'Sorry, there are no images matching your search query. Please try again.'
+//             );
+//             return;
+//         }
+            
+//         gallery.insertAdjacentHTML("beforeend", createMarkup(hits));
+
+//         if (page * 40 <= totalHits) {
+//             loadMoreBtn.classList.remove('is-hidden');
+
+//         } else {
+//             loadMoreBtn.classList.add('is-hidden');
+//             Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+//         }
+//         page += 1;
+//     }
+//     catch (err) {
+//         console.log(err);
+//         Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+//     }
+//     finally {
+//         searchForm.reset();
+//     }
+// }
--- src/js/back.js
+++ src/js/back.js
@@ -1,4 +1,29 @@
 import axios from 'axios';
 
-const BASE_URL = "https://pixabay.com/api/";
-const API_KEY = "40892694-b915ef9d6feb731a5d3c7d944";
+export class RequestImage {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '40892694-b915ef9d6feb731a5d3c7d944';
+  page = 1;
+  params = new URLSearchParams({
+    key: `${this.#API_KEY}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+  });
+    
+  async serviceImage(dataUser) {
+      
+        try {
+            const respImage = await axios.get(`${this.#BASE_URL}?${this.params}&q=${dataUser}&page=${this.page}`);
+            return respImage;
+        }
+        catch (err) {
+            console.log(err);
+      }
+    //     finally {
+    //         searchForm.reset();
+    //   }
+  }
+
+}
