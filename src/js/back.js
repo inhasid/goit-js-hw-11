f9967d16d20766019d2b0d77164cfb3a1a7f978c
inhasid <sidorchuk.inga@gmail.com>
2023-11-30 20:49:17 +0100

    commit10

--- src/js/back.js
+++ src/js/back.js
@@ -13,8 +13,8 @@ import axios from 'axios';
 //   });
     
  export default async function serviceImage(dataUser, page) {
-      BASE_URL = 'https://pixabay.com/api/';
-      API_KEY = '40892694-b915ef9d6feb731a5d3c7d944';
+      const BASE_URL = 'https://pixabay.com/api/';
+      const API_KEY = '40892694-b915ef9d6feb731a5d3c7d944';
         try {
           //const respImage = await axios.get(`${this.#BASE_URL}?${this.params}&q=${dataUser}&page=${this.page}`);
             const respImage = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${dataUser}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
